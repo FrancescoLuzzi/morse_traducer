5d43f4eab1c8a5aa89da4bd09b75358e8ea89e12
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-05-26 00:47:01 +0200

    usage of clap+derive to create a basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 use std::env;
 use std::fs::OpenOptions;
 use std::io::{self, Write};
+use std::str::FromStr;
+
+use clap::{self, Parser};
+
+#[derive(Debug)]
+enum OutputType {
+    Text,
+    Audio,
+}
+
+impl FromStr for OutputType {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "text" => Ok(OutputType::Text),
+            "audio" => Ok(OutputType::Audio),
+            _ => Err(format!("Type of output not found: {}", s.to_string())),
+        }
+    }
+}
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct MorseArgs {
+    /// Type of traduction from human readable text to morse:
+    /// -text
+    /// -audio
+    pub traduction_type: OutputType,
+
+    /// Name of the file to read, if the value is "-" read from stdin
+    #[clap(short, long)]
+    in_file: String,
+
+    /// Name of the file to read, if the value is "-" write to stdout
+    #[clap(short, long)]
+    out_file: String,
+}
 
 fn get_writer(arg: Option<String>) -> Box<dyn Write> {
     match arg.as_deref() {
@@ -16,6 +53,8 @@ fn get_writer(arg: Option<String>) -> Box<dyn Write> {
 }
 
 fn main() {
+    let args = MorseArgs::parse();
+    print!("{:?}", args);
     let arg = env::args().nth(1);
     let mut out_writer = get_writer(arg);
     out_writer
