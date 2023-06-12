67144dc834d7b0a230acbede643f7a055be3b1f6
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-06-12 23:29:48 +0200

    fixed notes combination

--- src/wav.rs
+++ src/wav.rs
@@ -1,7 +1,7 @@
-use std::fs::OpenOptions;
 use std::io::{self, Write};
 
 pub const SAMPLE_RATE: u32 = 44100;
+pub const AMPLITUDE: f32 = i16::MAX as f32;
 
 pub enum Volume {
     Silent,
@@ -39,37 +39,38 @@ pub mod notable_notes {
     pub const C0: Note = Note {
         frequency: 16.35_f32,
     };
+    pub const C4_SH: Note = Note { frequency: 277.18 };
 }
 
 impl Note {
-    pub fn combine(notes: &[Self], secs: usize, volume: &Volume) -> Vec<u8> {
+    pub fn combine(notes: &[Self], secs: usize, volume: &Volume) -> Vec<i16> {
         let nsamples = secs * SAMPLE_RATE as usize;
-        let mut buf: Vec<u8> = Vec::new();
-        let notes_number = notes.len() as u8;
+        let mut buf: Vec<i16> = Vec::new();
+        let notes_number = notes.len() as i16;
         for _ in 0..nsamples {
             buf.push(0);
         }
         for values in notes.iter().map(|note| note.audio_wave(secs, volume)) {
-            for (new_val, val) in buf.iter_mut().zip(values) {
+            for (new_val, val) in buf.iter_mut().zip(&values) {
                 *new_val += val / notes_number;
             }
         }
         buf
     }
 
-    pub fn audio_wave(&self, secs: usize, volume: &Volume) -> Vec<u8> {
+    pub fn audio_wave(&self, secs: usize, volume: &Volume) -> Vec<i16> {
         let nsamples = secs * SAMPLE_RATE as usize;
-        let mut buf: Vec<u8> = Vec::new();
+        let mut buf: Vec<i16> = Vec::new();
         for t in 0..nsamples {
             let s = match *volume {
-                Volume::Silent => 0_16,
+                Volume::Silent => 0_i16,
                 _ => {
                     let w = 2.0 * std::f32::consts::PI * self.frequency * t as f32;
                     let s = f32::sin(w / (SAMPLE_RATE as f32));
-                    f32::floor(u16::MAX as f32 * (volume.scaling() * s)) as i16
+                    f32::floor(AMPLITUDE * (volume.scaling() * s)) as i16
                 }
             };
-            buf.extend_from_slice(&make_bytes::<u16>(s as u16));
+            buf.push(s);
         }
         buf
     }
@@ -87,8 +88,8 @@ where
     b
 }
 
-fn write_wav(data: Vec<u8>, writer: &mut Box<dyn Write>) -> io::Result<()> {
-    let nsamples = data.len();
+fn write_wav(data: Vec<i16>, writer: &mut Box<dyn Write>) -> io::Result<()> {
+    let nsamples = data.len() * 2;
     writer.write_all(b"RIFF")?;
     let rsize = make_bytes::<u32>(20 + nsamples as u32); // added 20 for the rest of the header
     writer.write_all(&rsize)?; // WAVE chunk size
@@ -103,20 +104,22 @@ fn write_wav(data: Vec<u8>, writer: &mut Box<dyn Write>) -> io::Result<()> {
     writer.write_all(&make_bytes::<u16>(1))?; // number of channels
     writer.write_all(&make_bytes::<u32>(SAMPLE_RATE))?; // sample rate
     writer.write_all(&make_bytes::<u32>(SAMPLE_RATE))?; // data rate
-    writer.write_all(&make_bytes::<u16>(1))?; // block size
+    writer.write_all(&make_bytes::<u16>(2))?; // block size
     writer.write_all(&make_bytes::<u16>(16))?; // bits per sample
 
     // data chunk
     writer.write_all(b"data")?;
     writer.write_all(&make_bytes::<u32>(nsamples as u32))?; // data chunk size
-
-    writer.write_all(&data)?;
+    for half_word in data {
+        writer.write_all(&make_bytes(half_word as u16))?;
+    }
 
     writer.flush()
 }
 
 #[test]
 fn test_file() {
+    use std::fs::OpenOptions;
     fn get_writer(arg: &str) -> Box<dyn Write> {
         match arg {
             "-" => Box::new(io::stdout().lock()),
@@ -134,12 +137,11 @@ fn test_file() {
     let mut data_content = Vec::new();
     data_content.extend_from_slice(&notable_notes::A4.audio_wave(3, &Volume::Medium));
     data_content.extend_from_slice(&notable_notes::A4.audio_wave(3, &Volume::Silent));
-    data_content.extend_from_slice(&notable_notes::G0.audio_wave(3, &Volume::Low));
-    // need to generate data in i16 then pass it to
-    // data_content.extend_from_slice(&Note::combine(
-    //     &[notable_notes::C0, notable_notes::G0, notable_notes::E0],
-    //     3,
-    //     &Volume::High,
-    // ));
+    data_content.extend_from_slice(&notable_notes::A4.audio_wave(3, &Volume::Low));
+    data_content.extend_from_slice(&Note::combine(
+        &[notable_notes::A4, notable_notes::C4_SH, notable_notes::E0],
+        3,
+        &Volume::Medium,
+    ));
     write_wav(data_content, &mut get_writer("file.wav")).unwrap();
 }
